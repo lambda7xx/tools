ce9985d8a8e2025a0571f0c1e28c59b48f847bd7
lambda7xx <lambda7xx@gmail.com>
2023-09-30 14:29:50 +0000

    use immutable

--- parse1.cc
+++ parse1.cc
@@ -43,7 +43,7 @@ template <typename T>
     if(default_value.has_value()) {  // Use has_value() to check if there's a value
         parser.mArguments[parse_key].value = std::to_string(default_value.value());  // Convert the value to string
         parser.mArguments[parse_key].default_value = true;
-        parser.mArguments[key].is_store_true = is_store_true;
+        parser.mArguments[parse_key].is_store_true = is_store_true;
         //parser.mArguments[parse_key].is_store_true = true;
         return CmdlineArgRef<T>{parse_key, default_value.value()};
     } 
@@ -68,8 +68,9 @@ bool convert<bool>(std::string const &s) {
   return s == "true" || s == "1" || s == "yes";
 }
 
-void parse_args(ArgsParser & mArgs, int argc, const char **argv) {
+ArgsParser parse_args(const ArgsParser & mArgs, int argc, const char **argv) {
     int i  = 1;
+    ArgsParser result;
     while (i  < argc) {
       std::cout<<"i:"<<i<<" and argv[i]:"<<argv[i]<<"and argc:"<<argc<<std::endl;
       std::string key = parseKey(argv[i]);
@@ -77,19 +78,24 @@ void parse_args(ArgsParser & mArgs, int argc, const char **argv) {
         exit(1);
       }
       // Check if the key is a store_true argument
-        if(mArgs.mArguments.count(key) && mArgs.mArguments[key].is_store_true) {
-            mArgs.mArguments[key].value = "true"; // Set to true if the flag is present
+        if(mArgs.mArguments.count(key) && mArgs.mArguments.at(key).is_store_true) {
+           // mArgs.mArguments[key].value = "true"; // Set to true if the flag is present
+            result.mArguments[key] = mArgs.mArguments.at(key);
+            result.mArguments[key].value = "true";
             i++;
             continue; // Skip the next iteration as there's no value associated with this flag
         }
       // Check if we have a value for this argument
         if (i + 1 < argc && (argv[i + 1][0] != '-' || (argv[i + 1][0] == '-' && argv[i + 1][1] == '-'))) {
-            mArgs.mArguments[key].value = argv[i + 1];
+           // mArgs.mArguments[key].value = argv[i + 1];
+            result.mArguments[key] = mArgs.mArguments.at(key);
+            result.mArguments[key].value = argv[i + 1];
             i += 2; // Increment to skip the value in the next iteration
         } else {
             i++; // Move to the next argument
         }
     }
+    return result;
     //return mArgs;
   }
 
@@ -100,7 +106,8 @@ T get(const ArgsParser & parser , const CmdlineArgRef<T> &ref)  {
     if(parser.mArguments.count(key)) {
       std::cout<<"2 T get key:"<<key<<"and parser.mArguments.at(key).is_store_true:"<<parser.mArguments.at(key).is_store_true <<std::endl;
         if(parser.mArguments.at(key).is_store_true) {
-             return convert<T>(parser.mArguments.at(key).value.value_or("false"));
+              return true;
+             //return convert<T>(parser.mArguments.at(key).value.value_or("false"));
         } else if (parser.mArguments.at(key).default_value || parser.mArguments.at(key).value.has_value()) {
             return convert<T>(parser.mArguments.at(key).value.value());
         } 
@@ -130,15 +137,18 @@ int main() {
     ArgsParser args;
     auto batch_size_ref = add_argument(args, "--batch-size", std::optional<int>(32), "Size of each batch during training");
     auto ll_gpus_ref = add_argument<int>(args, "-ll:gpus", std::nullopt, "Number of GPUs to be used for training");
-    auto verbose_ref = add_argument(args, "--verbose", std::optional<bool>(false), "Whether to print verbose logs");
-    parse_args(args , 7, const_cast<const char **>(test_argv));
+    auto fusion_ref = add_argument(args, "--fusion", std::optional<bool>(true), "Whether to use fusion or not");
+
+    //auto verbose_ref = add_argument(args, "--verbose", std::optional<bool>(false), "Whether to print verbose logs");
+    ArgsParser result = parse_args(args , 7, const_cast<const char **>(test_argv));
 
   // args.parse_args(9, const_cast<char **>(test_argv));
-   std::cout<<"batch_size:"<<get(args, batch_size_ref)<<std::endl;
-    std::cout<<"ll_gpus:"<<get(args, ll_gpus_ref)<<std::endl;
-    bool is_verbose = get(args, verbose_ref);
+   std::cout<<"batch_size:"<<get(result, batch_size_ref)<<std::endl;
+    std::cout<<"ll_gpus:"<<get(result, ll_gpus_ref)<<std::endl;
+    std::cout<<"fusion:"<<get(result, fusion_ref)<<std::endl;
+   // bool is_verbose = get(result, verbose_ref);
 
-    std::cout<<"verbose:"<<is_verbose<<std::endl;
+  //  std::cout<<"verbose:"<<is_verbose<<std::endl;
   // std::cout << "batch_size: " << args.get(batch_size_ref) << std::endl;
   // std::cout << "learning_rate: " << args.get(learning_rate_ref) << std::endl;
   // std::cout << "fusion: " << args.get(fusion_ref) << std::endl;
