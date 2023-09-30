1ef22d60bf6a6e6b2073abd838b028b046b074d8
lambda7xx <lambda7xx@gmail.com>
2023-09-30 14:12:41 +0000

    parse init version0.2

--- parse1.cc
+++ parse1.cc
@@ -27,7 +27,8 @@ std::string parseKey(const std::string & arg) {
 struct Argument {
     std::optional<std::string> value;  // Change value type to optional<string>
     std::string description;
-    bool default_value ;
+    bool default_value = false;
+    bool is_store_true = false; // Add a new field to indicate whether the argument is store_true
 };
 
 struct ArgsParser {
@@ -36,15 +37,17 @@ struct ArgsParser {
 
 template <typename T>
   CmdlineArgRef<T> add_argument(ArgsParser & parser, const std::string & key, const std::optional<T> & default_value,
-                         const std::string &description) {
-    key = parseKey(key);
-    parser.mArguments[key].description = description;
+                         const std::string &description, bool is_store_true = false) {
+    std::string parse_key = parseKey(key);
+    parser.mArguments[parse_key].description = description;
     if(default_value.has_value()) {  // Use has_value() to check if there's a value
-        parser.mArguments[key].value = std::to_string(default_value.value());  // Convert the value to string
-        parser.mArguments[key].default_value = true;
-        return CmdlineArgRef<T>{key, default_value.value()};
+        parser.mArguments[parse_key].value = std::to_string(default_value.value());  // Convert the value to string
+        parser.mArguments[parse_key].default_value = true;
+        parser.mArguments[key].is_store_true = is_store_true;
+        //parser.mArguments[parse_key].is_store_true = true;
+        return CmdlineArgRef<T>{parse_key, default_value.value()};
     } 
-    return CmdlineArgRef<T>{key, T{}};
+    return CmdlineArgRef<T>{parse_key, T{}};
   }
 
 template <typename T>
@@ -65,23 +68,42 @@ bool convert<bool>(std::string const &s) {
   return s == "true" || s == "1" || s == "yes";
 }
 
-ArgsParser parse_args(, int argc, const char **argv) {
-    ArgsParser mArgs
-    for (int i = 1; i < argc; i += 2) {
+void parse_args(ArgsParser & mArgs, int argc, const char **argv) {
+    int i  = 1;
+    while (i  < argc) {
+      std::cout<<"i:"<<i<<" and argv[i]:"<<argv[i]<<"and argc:"<<argc<<std::endl;
       std::string key = parseKey(argv[i]);
       if (key == "help" || key == "h") {
         exit(1);
       }
-      mArgs.mArguments[key].value = argv[i + 1];
+      // Check if the key is a store_true argument
+        if(mArgs.mArguments.count(key) && mArgs.mArguments[key].is_store_true) {
+            mArgs.mArguments[key].value = "true"; // Set to true if the flag is present
+            i++;
+            continue; // Skip the next iteration as there's no value associated with this flag
+        }
+      // Check if we have a value for this argument
+        if (i + 1 < argc && (argv[i + 1][0] != '-' || (argv[i + 1][0] == '-' && argv[i + 1][1] == '-'))) {
+            mArgs.mArguments[key].value = argv[i + 1];
+            i += 2; // Increment to skip the value in the next iteration
+        } else {
+            i++; // Move to the next argument
+        }
+
+      //mArgs.mArguments[key].value = argv[i + 1];
     }
-    return mArgs;
+    //return mArgs;
   }
 
 template <typename T>
 T get(const ArgsParser & parser , const CmdlineArgRef<T> &ref)  {
     std::string key = ref.key;
+    std::cout<<"1 T get key:"<<key<<std::endl;
     if(parser.mArguments.count(key)) {
-        if(parser.mArguments.at(key).default_value || parser.mArguments.at(key).value.has_value()) {
+      std::cout<<"2 T get key:"<<key<<"and parser.mArguments.at(key).is_store_true:"<<parser.mArguments.at(key).is_store_true <<std::endl;
+        if(parser.mArguments.at(key).is_store_true) {
+             return convert<T>(parser.mArguments.at(key).value.value_or("false"));
+        } else if (parser.mArguments.at(key).default_value || parser.mArguments.at(key).value.has_value()) {
             return convert<T>(parser.mArguments.at(key).value.value());
         } 
     }
@@ -95,15 +117,22 @@ int main() {
                              "100",
                               "-ll:gpus",
                              "6",
-                             "fusion",
-                             "false"};
+                             "--fusion",
+                             "false",
+                             "--verbose"};
+    
     ArgsParser args;
-    auto batch_size_ref = add_argument(args, "--batch-size", std::optional<int>(32), "Size of each batch during training");
-    auto ll_gpus_ref = add_argument<int>(args, "-ll:gpus", std::nullopt, "Number of GPUs to be used for training");
-    parse_args(args, 7, const_cast<char **>(test_argv));
+    auto batch_size_ref = add_argument(args, "--batch-size", std::optional<int>(32), "Size of each batch during training",false);
+    auto ll_gpus_ref = add_argument<int>(args, "-ll:gpus", std::nullopt, "Number of GPUs to be used for training",false);
+    auto verbose_ref = add_argument(args, "--verbose", std::optional<bool>(false), "Whether to print verbose logs", true);
+    parse_args(args , 8, const_cast<const char **>(test_argv));
+
   // args.parse_args(9, const_cast<char **>(test_argv));
    std::cout<<"batch_size:"<<get(args, batch_size_ref)<<std::endl;
     std::cout<<"ll_gpus:"<<get(args, ll_gpus_ref)<<std::endl;
+    bool is_verbose = get(args, verbose_ref);
+
+    std::cout<<"verbose:"<<is_verbose<<std::endl;
   // std::cout << "batch_size: " << args.get(batch_size_ref) << std::endl;
   // std::cout << "learning_rate: " << args.get(learning_rate_ref) << std::endl;
   // std::cout << "fusion: " << args.get(fusion_ref) << std::endl;
---
+++ run.sh
@@ -0,0 +1,2 @@
+g++ -std=c++17 parse1.cc > x.log 2>&1 
+code x.log 
